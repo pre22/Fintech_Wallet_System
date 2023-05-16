c3395aed5a02188cb06621fb02727c6a4acc76d3
Precious Chika <43546663+pre22@users.noreply.github.com>
2023-05-16 15:31:07 +0100

    added login and logout

--- platform_wallet/settings.py
+++ platform_wallet/settings.py
@@ -136,3 +136,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 SWEETIFY_SWEETALERT_LIBRARY = 'sweetalert2'
+
+LOGIN_REDIRECT_URL = 'dashboard'
+
+LOGOUT_REDIRECT_URL = 'home'
