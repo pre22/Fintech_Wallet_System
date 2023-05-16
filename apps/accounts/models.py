014a6ea6528ffbd319c319effd2438b597837bbe
Precious Chika <43546663+pre22@users.noreply.github.com>
2023-05-16 13:18:20 +0100

    models: working on custom user models

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -5,15 +5,20 @@ import uuid
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-from platform_wallet.managers import CustomUserManager
+from django.utils.translation import gettext_lazy as _
 from django.utils.text import slugify
+
+from platform_wallet.managers import CustomUserManager
 
 
 class CustomUser(AbstractUser):
 
     username = None
-    uid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    date_of_birth = models.DateField(_("date of birth"), max_length=150, blank=False)
+    uid = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False)
+    date_of_birth = models.DateField(
+        _("date of birth"), max_length=150, blank=False)
+    slug = models.SlugField(max_length=200, unique=True)
     verified = models.BooleanField(_("verified"), default=False)
 
     objects = CustomUserManager()
