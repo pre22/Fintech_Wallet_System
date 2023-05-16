97b0f68b8e8a6038c47c304ec5a05ba28178d8a3
Precious Chika <43546663+pre22@users.noreply.github.com>
2023-05-16 13:11:53 +0100

    working on custom user model

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,3 +1,29 @@
+import random
+import string
+import uuid
+
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from platform_wallet.managers import CustomUserManager
+from django.utils.text import slugify
+
+
+class CustomUser(AbstractUser):
+
+    username = None
+    uid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    date_of_birth = models.DateField(_("date of birth"), max_length=150, blank=False)
+    verified = models.BooleanField(_("verified"), default=False)
+
+    objects = CustomUserManager()
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify("{}-{}-{}".format(self.first_name, self.last_name,
+                            ''.join(random.choices(string.ascii_uppercase + string.digits, k=8))))
+        super(CustomUser, self).save(*args, **kwargs)
+
+    def __str__(self):
+        if self.first_name and self.last_name:
+            return "{} {}".format(self.first_name, self.last_name)
+        return self.email
