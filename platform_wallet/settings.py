f0115c3b4eaabb861906c500b758ef617c4aa221
Precious Chika <43546663+pre22@users.noreply.github.com>
2023-05-16 15:05:40 +0100

    added sweetify config to settings file

--- platform_wallet/settings.py
+++ platform_wallet/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'sweetify',
 
     'apps.accounts.apps.AccountsConfig',
     'apps.wallets.apps.WalletsConfig',
@@ -135,3 +137,5 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SWEETIFY_SWEETALERT_LIBRARY = 'sweetalert2'
