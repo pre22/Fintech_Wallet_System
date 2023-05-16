9e437b2972d5d4aca46c4cb5060e5692df5a4809
Precious Chika <43546663+pre22@users.noreply.github.com>
2023-05-16 15:28:49 +0100

    added route

--- platform_wallet/urls.py
+++ platform_wallet/urls.py
@@ -17,7 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.pages.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
